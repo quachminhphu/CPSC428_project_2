db4ec211c5a1afe7beb5a4f9787dc17015177936
quachminhphu <phu.quach.13@cnu.edu>
2016-03-28 13:02:26 -0400

    Update 1

--- src/Hash.java
+++ src/Hash.java
@@ -24,5 +24,43 @@ public class Hash {
 		return totalDiff;
 	}
 	
+	public static void printByteArrayInBinary(byte [] array)
+	{
+	 	System.out.print("[");
+	   	for(int i = 0; i < array.length-1; i++)
+	   {
+	   		for(int j = 7; j >= 0; j--)
+	       	System.out.print(
+					Integer.toBinaryString((array[i]>>j)&0x01));
+	        System.out.print(", ");
+	    }
+	    for(int j = 7; j >= 0; j--)
+	    	System.out.print(
+				Integer.toBinaryString((array[array.length-1]>>j)&0x01));
+	    System.out.println("]");
+	}
+
 	
+	public static byte [] xor(byte [] b1, byte [] b2)
+	{
+		byte [] r = new byte[b1.length];
+	        
+	   for(int i=0; i<b1.length && i<b2.length; i++)
+	   	r[i] = (byte) (b1[i]^b2[i]);
+	 	return r;
+	}
+	
+	public static int countOnes(byte [] b)
+	{
+	    int count = 0;
+	    for(int i = 0; i < b.length; i++)
+	    {
+	    	for(int j = 0; j < 8; j++)
+	    	{
+	    	count += (b[i]>>j) & 0x01;
+	    	}
+	    }
+	    return count;
+	}
+
 }
